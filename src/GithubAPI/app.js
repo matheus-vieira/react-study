5ec79bbdb1ba2b5be7e6c2930269602fc498c05a
Matheus <matheus@eficilog.com>
2018-08-21 12:20:50 -0300

    showing a table with datables

--- src/GithubAPI/app.js
+++ src/GithubAPI/app.js
@@ -6,6 +6,15 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    next();
+}
+app.use(allowCrossDomain);
 app.use('/api', githubRouter);
 app.get('/', (req, res) => res.send('Welcome to my API'));
 app.listen(port, () => console.log('Gulp is running my app on PORT: ' + port));
---
+++ src/front/src/Table.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+
+const $ = require('jquery');
+$.DataTable = require('datatables.net-bs4');
+require('datatables.net-responsive-bs4');
+
+const columns = [
+    {
+        title: '',
+        data: 'avatar',
+        render: (d) => `<img src="${d}" alt="User avatar" title="User Avatar" width="30" height="30" />`
+    },
+    {
+        title: 'Login',
+        data: 'login'
+    },
+    {
+        title: 'Actions',
+        data: null,
+        render: function ( data, type, row ) {
+            return '<button type="button" class="btn btn-outline-primary">Primary</button>';
+        }
+    }
+];
+
+function reloadTableData(names) {
+    const table = $('.data-table-wrapper').find('table').DataTable();
+    table.clear();
+    table.rows.add(names);
+    table.draw();
+}
+
+function updateTable(names) {
+    const table = $('.data-table-wrapper').find('table').DataTable();
+    let dataChanged = false;
+    table.rows().every(function () {
+        const oldNameData = this.data();
+        const newNameData = names.find((nameData) => {
+            return nameData.name === oldNameData.name;
+        });
+        if (oldNameData.nickname !== newNameData.nickname) {
+            dataChanged = true;
+            this.data(newNameData);
+        }
+       return true;
+    });
+
+    if (dataChanged) {
+        table.draw();
+    }
+}
+
+class Table extends Component {
+    componentDidMount() {
+        $(this.refs.main).DataTable({
+            dom: '<"data-table-wrapper"t>',
+            data: this.props.names,
+            columns,
+            ordering: false,
+            ajax: {
+                dataSrc: "",
+                url: 'http://localhost:8000/api/users'
+            },
+        });
+    }
+
+    componentWillUnmount(){
+       $('.data-table-wrapper').find('table').DataTable().destroy(true);
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if (nextProps.names.length !== this.props.names.length) {
+            reloadTableData(nextProps.names);
+        } else {
+            updateTable(nextProps.names);
+        }
+        return false;
+    }
+        
+
+    render() {
+        return (
+            <div>
+                <table ref="main" className="table table-dark table-bordered table-striped table-hover table-sm" />
+            </div>);
+    }
+}
+
+export default Table;
